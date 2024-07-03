a045740699868b23b189bace20038aebf1c9eb31
ahkfgu <161267145+ahkfgu@users.noreply.github.com>
2024-07-03 09:51:27 +0800

    Add files via upload

---
+++ user_config.py
@@ -0,0 +1,59 @@
+open_update = True
+source_file = "demo.txt"
+final_file = "result.txt"
+favorite_list = [
+    "安徽卫视",
+    "安徽经济生活",
+    "安徽公共",
+    "安徽影视",
+    "安徽综艺体育",
+    "阜阳新闻",
+    "阜阳公共",
+    "阜阳教育",
+    "阜阳都市",
+    "CCTV1",
+    "CCTV3",
+    "CCTV5",
+    "CCTV5+",
+    "CCTV6",
+    "CCTV13",
+    "山东体育",
+    "五星体育",
+    "江苏卫视",
+    "浙江卫视",
+    "湖南卫视",
+    "凤凰卫视",
+    "凤凰资讯",
+    "凤凰中文",
+    "劲爆体育",
+    "超级体育",
+    "精品体育",
+    "纬来体育",
+    "咪咕体育",
+    "重温经典",
+    "CHC家庭影院",
+    "CHC动作电影",
+    "CHC影迷电影",
+]
+open_online_search = True
+favorite_page_num = 5
+default_page_num = 3
+urls_limit = 15
+open_sort = True
+response_time_weight = 0.5
+resolution_weight = 0.5
+recent_days = 30
+ipv_type = "ipv4"
+domain_blacklist = [""]
+url_keywords_blacklist = []
+open_subscribe = True
+subscribe_urls = [
+    "https://gitee.com/T-GU/kk/raw/master/teach.json",
+    "https://gitee.com/sangni2024/mingge/raw/master/1949.txt",
+    "https://m3u.ibert.me/txt/fmml_dv6.txt",
+    "https://m3u.ibert.me/txt/o_cn.txt",
+    "https://m3u.ibert.me/txt/j_iptv.txt",
+]
+open_multicast = True
+region_list = ["all"]
+open_proxy = True
